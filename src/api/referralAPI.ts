793289382f6d113f3e659b94554cf86e7df6a7d9
totop716 <totop716@gmail.com>
2023-09-08 15:45:54 -0400

    add function to get referral rebates

--- src/api/referralAPI.ts
+++ src/api/referralAPI.ts
@@ -9,6 +9,19 @@ import { ContractTransaction } from 'ethers'
  */
 export class ReferralAPI extends BaseAPI {
 	/**
+	 * Returns a promise containing the array of tokens and amounts for referral fees.
+	 * @param referrer {string} The address of the referrer.
+	 * @param vault {string} The addres of the vault addresses.
+	 * @returns {Promise<[string[], bigint[]] & { tokens: string[]; rebates: bigint[] }>} Promise the array of tokens and amounts for referral fees.
+	 */
+	async getRebates(
+		referrer: string
+	): Promise<[string[], bigint[]] & { tokens: string[]; rebates: bigint[] }> {
+		const referralContract = this.premia.contracts.getReferralContract()
+		return referralContract.getRebates(referrer)
+	}
+
+	/**
 	 * Encodes the claimRebate parameters into a transaction that can be broadcasted to the provider network.
 	 *
 	 * @param {string[]} tokens - array of addresses of tokens to claim.
