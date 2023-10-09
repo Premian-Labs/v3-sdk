3c209f826d3e643794f71d2d167aefb575d4c792
froggiedev <froggie@premia.finance>
2023-10-09 13:50:40 +0200

    Move abi to root folder

--- scripts/copyAbiToDist.ts
+++ scripts/copyAbiToDist.ts
@@ -1,7 +1,7 @@
 const fse = require('fs-extra')
 
 const srcDir = `node_modules/@premia/v3-abi/abi`
-const destDir = `dist/abi`
+const destDir = `abi`
 
 try {
 	fse.copySync(srcDir, destDir, { overwrite: true })
